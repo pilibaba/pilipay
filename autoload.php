c754cb9a3d143d6dd5a822f949cf25d9a6940903
Clarence <clarence-pan@foxmail.com>
2015-11-27 00:02:49 +0800

    fix requirement orders

--- autoload.php
+++ autoload.php
@@ -1,11 +1,11 @@
 <?php
 
 // require all Pilipay's files
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayCurl.php');
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayError.php');
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayGood.php');
 require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayLogger.php');
 require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayModel.php');
+require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayError.php');
+require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayCurl.php');
+require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayGood.php');
 require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayOrder.php');
 require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayPayResult.php');
 
