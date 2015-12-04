63b7a9dca3f14d3d47ca93b04047acbbac612228
Clarence <clarence-pan@foxmail.com>
2015-12-04 13:53:25 +0800

    avoid duplicated requires

--- autoload.php
+++ autoload.php
@@ -1,11 +1,11 @@
 <?php
 
 // require all Pilipay's files
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayLogger.php');
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayModel.php');
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayError.php');
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayCurl.php');
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayGood.php');
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayOrder.php');
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayPayResult.php');
+!class_exists('PilipayLogger', false) and require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayLogger.php');
+!class_exists('PilipayModel', false) and require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayModel.php');
+!class_exists('PilipayError', false) and require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayError.php');
+!class_exists('PilipayCurl', false) and require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayCurl.php');
+!class_exists('PilipayGood', false) and require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayGood.php');
+!class_exists('PilipayOrder', false) and require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayOrder.php');
+!class_exists('PilipayPayResult', false) and require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PilipayPayResult.php');
 
